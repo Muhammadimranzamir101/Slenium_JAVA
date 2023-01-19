88154a35a24aa06e6877dba8a8145fb9cf07f77d
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-01-19 16:55:15 +0500

    first test case in selenium

---
+++ src/main/java/selenium/intro.java
@@ -0,0 +1,16 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class intro {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.close();
+
+    }
+}
