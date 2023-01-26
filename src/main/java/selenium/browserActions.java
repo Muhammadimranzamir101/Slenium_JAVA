2928cb60abac0d93cae79908264d332898c6d503
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-01-26 18:09:33 +0500

    browser actions in selenium

---
+++ src/main/java/selenium/browserActions.java
@@ -0,0 +1,19 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class browserActions {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://google.com");
+        driver.navigate().to("https://amazon.com");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.findElement(By.cssSelector("input[aria-label=\"Search Amazon\"]")).sendKeys("Books");
+        driver.navigate().refresh();
+        driver.close();
+    }
+}
