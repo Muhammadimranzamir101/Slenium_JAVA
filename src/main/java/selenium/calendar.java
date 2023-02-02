2702396cf31908ac8df7b65028e6a24a5a2fbb71
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-02-02 14:35:10 +0500

    calendar handling in selenium

---
+++ src/main/java/selenium/calendar.java
@@ -0,0 +1,46 @@
+package selenium;
+
+import org.jetbrains.annotations.NotNull;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class calendar {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.booking.com");
+        driver.findElement(By.cssSelector("button[data-testid*=\"start\"]")).click();
+
+        selectRoundTrip(driver, 15,20);
+        //selectReturnDateAfterNDays(driver,5);
+        Thread.sleep(5000);
+        driver.close();
+    }
+
+    public static void selectRoundTrip(@NotNull WebDriver driver, int startDate, int endDate)
+    {
+        List<WebElement> totalDays = driver.findElements(By.xpath("//td/span[not(contains(@class,\"f5774da2d4\"))]"));
+
+        for(WebElement day : totalDays)
+        {
+            String  date = day.getText();
+            if(Integer.parseInt(date) == startDate)
+                day.click();
+            if(Integer.parseInt(date) == endDate)
+            {
+                day.click();
+                break;
+            }
+        }
+    }
+
+    public static void selectReturnDateAfterNDays(WebDriver driver, int day)
+    {
+        driver.findElement(By.xpath("(//td/span[not(contains(@class,\"f5774da2d4\"))])["+1+"]")).click();
+        driver.findElement(By.xpath("(//td/span[not(contains(@class,\"f5774da2d4\"))])["+(day+1)+"]")).click();
+    }
+}
