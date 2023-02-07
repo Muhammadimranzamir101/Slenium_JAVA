92747b31a68e59c6fdc6fbdb1d4e66249d5eddc4
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-02-07 20:24:38 +0500

    Fluent Wait in selenium

---
+++ src/main/java/selenium/fluentWait.java
@@ -0,0 +1,44 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.function.Function;
+
+public class fluentWait {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+        driver.findElement(By.cssSelector("#start>button")).click();
+
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class);
+
+        WebElement label = wait.until(new Function<WebDriver, WebElement>() {
+            @Override
+            public WebElement apply(WebDriver driver) {
+                if(driver.findElement(By.cssSelector("[id='finish'] h4")).isDisplayed())
+                {
+                    return driver.findElement(By.cssSelector("[id='finish'] h4"));
+                }
+                else {
+                    return null;
+                }
+            }
+        });
+
+        //WebElement label = wait.until(Driver -> Driver.findElement(By.cssSelector("[id='finish'] h4")));
+
+        System.out.println("Text: "+label.getText());
+        driver.close();
+    }
+}
