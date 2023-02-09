ff04ad2776c5c3eb1cf778c11a57153fe34a31a2
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-02-09 14:28:28 +0500

    actions in selenium

---
+++ src/main/java/selenium/Action.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Action {
+    public static void main(String[] args) {
+        WebDriver chrome = new ChromeDriver();
+        chrome.get("https://amazon.com");
+        WebElement signIn = chrome.findElement(By.cssSelector("#nav-link-accountList"));
+        WebElement search = chrome.findElement(By.cssSelector("#twotabsearchtextbox"));
+        Actions a = new Actions(chrome);
+
+        //Hover
+        a.moveToElement(signIn).build().perform();
+        //Right Click
+        a.contextClick(signIn).build().perform();
+        //Enter Text in Capital
+        a.click(search).keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+    }
+}
