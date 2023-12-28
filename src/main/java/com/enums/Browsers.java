537933143bb1316a5c9c6f01a9c23b3e2b66c2f8
Muhammadimranzamir101 <muhammadimranzamir786@gmail.com>
2023-12-28 19:49:04 +0500

    add Browsers enum

---
+++ src/main/java/com/enums/Browsers.java
@@ -0,0 +1,24 @@
+package com.enums;
+
+import lombok.Getter;
+
+@Getter
+public enum Browsers {
+
+    /**
+     * Chrome Browser
+     */
+    CHROME,
+    /**
+     * Edge browser.
+     */
+    EDGE,
+    /**
+     * Firefox browser.
+     */
+    FIREFOX,
+    /**
+     * Firefox browser.
+     */
+    SAFARI,
+}
